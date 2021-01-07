fad36bf64ce271e1a7b8ea07996f17711ede4934
Calebe <calebebraga08@gmail.com>
2021-01-07 17:15:58 -0300

    Add Home Page

--- front-web/src/Navbar/index.tsx
+++ front-web/src/Navbar/index.tsx
@@ -5,7 +5,7 @@ function Navbar(){
     return(
         <nav className ="main-navbar" >
             <Logo/>
-            <a href="home" className = "logo-text">DS Deliver</a>
+            <a href="home" className = "logo-text">DS Delivery</a>
         </nav>
     )
 }
